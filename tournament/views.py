5959cdb2b617913b651b371ad96ccbde02f1e151
Dariusz Kmiecik <dkmieci1@intercars.eu>
2020-09-26 13:44:13 +0200

    [ADD] Added page /tournaments Page has table with all players and Teams

--- tournament/views.py
+++ tournament/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from tournament.models import Team, Player
 
 # Create your views here.
+
+class TournamentViews(View):
+    def get(self, request, *args, **kwargs):
+        players = Player.objects.all
+        return render(request, 'tournament/tournament.html', context={'players': players})
